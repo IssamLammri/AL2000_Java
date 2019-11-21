fc816006f4298e2826316d4115570a6400fb05b1
troubadourjimmy <zhangxubeinicolas@gmail.com>
2019-11-21 18:41:58 +0100

    hhhhh

--- AL2000/src/src/test.java
+++ AL2000/src/src/test.java
@@ -24,12 +24,12 @@ public class test {
 
 		int choix = 0;
 
-		System.out.println("------------Bonjour------------");
-		System.out.println("Veuillez choisir votre moyen d'accés à notre application ");
+		System.out.println("------------Bonjourjj------------");
+		System.out.println("Veuillez choisir votre moyen d'accé–Ÿ ï¿½ notre application ");
 
 		System.out.println("	1 =>  Client ");
-		System.out.println("	2 =>  Abonné ");
-		System.out.println("	3 =>  Clients(Abonnés) ");
+		System.out.println("	2 =>  Abonnï¿½ ");
+		System.out.println("	3 =>  Clients(Abonné–Ÿ) ");
 		System.out.println("	4 =>  Les Locations ");
 		System.out.println("	5 =>  Les DVD(s) ");
 		System.out.println("	6 =>  Les Films ");
@@ -40,7 +40,7 @@ public class test {
 		while (choix != 99) {
 			if (choix == 1) {
 				System.out.println("------Bienvenu chez vous Dans notre Application AL2000------");
-				System.out.println("Voici la liste des foncationnalité que vous avez droit en tant que client");
+				System.out.println("Voici la liste des foncationnalitï¿½ que vous avez droit en tant que client");
 				System.out.println("	1 =>  Rechercher un film 	");
 				System.out.println("	2 =>  Louer un film		");
 				System.out.println("	3 =>  S'abonner un film		");
@@ -59,13 +59,13 @@ public class test {
 						choix1 = 5;
 					} else if (choix1 == 2) {
 						System.out.println(
-								"  Afin de louer un Film , il faut tout d'abord que vous remplissez vos données  :: ");
-						System.out.println("  Commençant par la carte Bancaire :: ");
+								"  Afin de louer un Film , il faut tout d'abord que vous remplissez vos donné–‘s  :: ");
+						System.out.println("  Commené°nt par la carte Bancaire :: ");
 						String TitulaireCarte;
 						int cvv;
 						Date date = null;
 						Double Montant;
-						System.out.println(" Veuillez Insérer Le titulaire de la carte ");
+						System.out.println(" Veuillez Insé–žer Le titulaire de la carte ");
 						TitulaireCarte = sc.nextLine();
 						System.out.println("Saisissez une date (JJ/MM/AAAA) :");
 						String str = sc.nextLine();
@@ -86,23 +86,23 @@ public class test {
 								str = sc.nextLine();
 							}
 						}
-						System.out.println(" Veuillez Insérer son CVV");
+						System.out.println(" Veuillez Insé–žer son CVV");
 						cvv = Integer.parseInt(sc.nextLine());
-						System.out.println(" Veuillez Insérer Le Montant de cette Carte ");
+						System.out.println(" Veuillez Insé–žer Le Montant de cette Carte ");
 						Montant = Double.parseDouble(sc.nextLine());
 
 						Carte_Bleu cBC = new Carte_Bleu(TitulaireCarte, cvv, date, Montant);
 
-						System.out.println("  Passant à vos propres informations :: ");
+						System.out.println("  Passant ï¿½ vos propres informations :: ");
 						String Nom;
 						String Prenom;
 						String Adresse;
 						Date DuneDate1;
-						System.out.println(" Veuillez Insérer Votre Nom ");
+						System.out.println(" Veuillez Insé–žer Votre Nom ");
 						Nom = sc.nextLine();
-						System.out.println(" Veuillez Insérer Votre Prenom ");
+						System.out.println(" Veuillez Insé–žer Votre Prenom ");
 						Prenom = sc.nextLine();
-						System.out.println(" Veuillez Insérer Votre Adresse ");
+						System.out.println(" Veuillez Insé–žer Votre Adresse ");
 						Adresse = sc.nextLine();
 
 						UpdateArrayListClients();
@@ -123,7 +123,7 @@ public class test {
 						System.out.println("veuillez introduire le numero de DVD que vous voulez louer");
 						int NumDVD = Integer.parseInt(sc.nextLine());
 						while (NumDVD > DVD.size() || NumDVD < 1) {
-							System.out.println("choix invalide , veuillez réessayer ");
+							System.out.println("choix invalide , veuillez ré–‘ssayer ");
 							NumDVD = Integer.parseInt(sc.nextLine());
 						}
 
@@ -138,7 +138,7 @@ public class test {
 						break;
 					} else {
 						System.out.println(
-								"Veuillez avez entrer un choix invalide :/ , Veuillez réessayer encore une fois : ");
+								"Veuillez avez entrer un choix invalide :/ , Veuillez ré–‘ssayer encore une fois : ");
 						choix1 = sc.nextInt();
 					}
 				}
@@ -146,7 +146,7 @@ public class test {
 			} else if (choix == 2) {
 
 			} else if (choix == 3) {
-				System.out.println("La liste des Clients Abonnés ");
+				System.out.println("La liste des Clients Abonné–Ÿ ");
 				Client mohammed = new Client();
 
 				ArrayList<Object> Clients = mohammed.GetAllClients();
@@ -191,7 +191,7 @@ public class test {
 			}
 
 			else {
-				System.out.println("Veuillez avez entrer un choix invalide :/ , Veuillez réessayer encore une fois : ");
+				System.out.println("Veuillez avez entrer un choix invalide :/ , Veuillez ré–‘ssayer encore une fois : ");
 				choix = sc.nextInt();
 			}
 		}
